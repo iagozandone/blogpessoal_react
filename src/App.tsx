e5476ee5095355804dd1609c6f42f4e15af8db0f
Iago Zandone <iagozandone@gmail.com>
2025-06-23 22:10:26 -0300

    :sparkles: implementação do cadastrar e login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer"
 import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
+import Cadastro from "./pages/cadastro/Cadastro";
+import Login from "./pages/login/Login";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <>
+    <AuthProvider>
+      <BrowserRouter>
       <Navbar/>
-      <Home/>
+      <div className="min-h-[80vh]">
+          <Routes>
+              <Route 
+                  path="/" element={<Login/>}
+              />
+              <Route 
+                  path="/home" element={<Home/>}
+              /> 
+              <Route 
+                  path="/cadastro" element={<Cadastro/>}
+              />
+              <Route 
+                  path="/login" element={<Login/>}
+              />
+          </Routes>
+        </div>
       <Footer/>
+      </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
